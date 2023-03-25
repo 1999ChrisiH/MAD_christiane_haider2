35eecb97a866b1b2559349262d55efb1b14339dd
1999ChrisiH <christiane.haider@stud.fh-campuswien.ac.at>
2023-03-25 13:57:52 +0100

    LD02 deleted unnecessary commands

--- app/src/main/java/com/example/movieproject/MainActivity.kt
+++ app/src/main/java/com/example/movieproject/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.movieproject
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -21,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
